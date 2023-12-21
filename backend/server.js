dfd1b90fb63c4d8067136b9d07e14e85055976e8
Naman Pandey <2492004namanpandey@gmail.com>
2023-12-21 22:26:01 +0530

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+const server = require("http").createServer(app);
+const {Server} = require("socket.io");
+
+const io = new Server(server);
+
+// routes
+app.get("/",(req,res)=>{
+    res.send("This is mern realtime whiteboard sharing app official server.")
+})
+
+io.on("connection", (socket) => {
+    console.log("User connected");
+})
+
+const port = process.env.port || 5000;
+
+server.listen(port, ()=> console.log("server is running on http://localhost:5000"));
